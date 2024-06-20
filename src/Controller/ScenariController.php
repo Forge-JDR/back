ee55e28ad423cf55fc065e05141874c9b35d5247
BrunoWOLFF-Ynov <bruno.wolffdumontel@ynov.com>
2024-06-20 14:47:19 +0200

    Update ScenariController.php

--- src/Controller/ScenariController.php
+++ src/Controller/ScenariController.php
@@ -4,9 +4,15 @@ namespace App\Controller;
 
 use App\Repository\ScenariRepository;
 use App\Entity\Scenari;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ScenariController extends AbstractController
 {
@@ -27,11 +33,9 @@ class ScenariController extends AbstractController
     }
 
     #[Route('/api/scenari', name: "scenari.create", methods: ["POST"])]
-    public function createWiki(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    public function createScenari(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
     {
         $scenari = $serializer->deserialize($request->getContent(), Scenari::class, 'json');
-        $scenari->setCreatedAt(new \DateTimeImmutable());
-        $scenari->setStatus('active');
         $entityManager->persist($scenari);
         $entityManager->flush();
 
